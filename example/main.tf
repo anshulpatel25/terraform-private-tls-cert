82dce7bcad072cd707ee4d9b917cf2a30656f7cb
anshulpatel25 <anshulpatel25@gmail.com>
2018-12-03 23:26:51 +0530

    Added variable support for example

--- example/main.tf
+++ example/main.tf
@@ -1,18 +1,30 @@
 module "ca" {
   source             = "../modules/ca"
-  ca_common_name     = "warmonger.in"
-  organization_name  = "Warmonger, Inc."
-  ca_public_key_path = "D:\\GitHub\\ca.pem"
+  ca_common_name     = "${var.common_name}"
+  organization_name  = "${var.organization_name}"
+  ca_public_key_path = "D:\\GitHub\\ca.crt"
 }
 
 module "api-cert" {
   source                = "../modules/certificate"
-  common_name           = "warmonger.in"
-  organization_name     = "Warmonger, Inc."
+  common_name           = "${var.common_name}"
+  organization_name     = "${var.organization_name}"
   cert_private_key_path = "D:\\GitHub\\api.warmonger.in.pem"
   dns_names             = ["api.warmonger.in"]
   ca_key_algorithm      = "${module.ca.ca_key_algorithm}"
   ca_private_key_pem    = "${module.ca.ca_private_key_pem}"
   ca_cert_pem           = "${module.ca.ca_cert_pem}"
   cert_public_key_path  = "D:\\GitHub\\api.warmonger.in.crt"
+}
+
+module "frontend-cert" {
+  source                = "../modules/certificate"
+  common_name           = "${var.common_name}"
+  organization_name     = "${var.organization_name}"
+  cert_private_key_path = "D:\\GitHub\\www.warmonger.in.pem"
+  dns_names             = ["www.warmonger.in"]
+  ca_key_algorithm      = "${module.ca.ca_key_algorithm}"
+  ca_private_key_pem    = "${module.ca.ca_private_key_pem}"
+  ca_cert_pem           = "${module.ca.ca_cert_pem}"
+  cert_public_key_path  = "D:\\GitHub\\www.warmonger.in.crt"
 }
---
+++ example/variables.tf
@@ -0,0 +1,9 @@
+variable "common_name" {
+  description = "Common Name"
+  default     = "warmonger.in"
+}
+
+variable "organization_name" {
+  description = "Organization Name"
+  default     = "Warmonger, Inc."
+}
